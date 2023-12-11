1cc00f2f07440ac3617a5f32a4af7285d517851d
LuanaArimitsu <142428639+LuanaArimitsu@users.noreply.github.com>
2023-12-11 18:37:40 -0300

    criando repositorio

--- Exercicio95.java
+++
@@ -1,9 +0,0 @@
-package logica_programacao;
-
-public class Exercicio95 {
-
-    public static void main (String[] args){
-
-
-    }
-}
--- src/logica_programacao/Exercicio70.java
+++ src/logica_programacao/Exercicio70.java
@@ -1,2 +1,21 @@
-package logica_programacao;public class Exercicio70 {
+package logica_programacao;
+
+import java.util.Scanner;
+
+public class Exercicio70 {
+    public static void main (String[]args){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        int numero = leitor.nextInt();
+
+        System.out.println("Tabuada do número " + numero + ":");
+
+        for (int i = 1; i <= 10; i++) {
+            int resultado = numero * i;
+            System.out.println(numero + " x " + i + " = " + resultado);
+        }
+
+        leitor.close();
+    }
 }
